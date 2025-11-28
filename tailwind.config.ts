6e3e3717f50e0cbbc624c5d037aee9722d7bf843
Wahaj Mansoor <wahajmansoorcom@gmail.com>
2025-11-28 22:03:32 +0000

    font family DM Sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"DM Sans"', 'sans-serif'],
+        headline: ['"DM Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
