d6dcae9ed3212e470592148c37b985fbeac4db57
Wahaj Mansoor <wahajmansoorcom@gmail.com>
2025-11-28 22:53:30 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,10 @@ const nextConfig: NextConfig = {
     ],
   },
   // This is to allow cross-origin requests from the development environment.
-  allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+  allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+    'https://6000-firebase-studio-1764365669384.cluster-isls3qj2gbd5qs4jkjqvhahfv6.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
