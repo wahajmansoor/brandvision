b297aa74e98d2dbcb46a2c57a9dcfb4b4e0bafd5
Wahaj Mansoor <wahajmansoorcom@gmail.com>
2025-11-28 21:59:29 +0000

    make sure dark mode and light mode working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,21 @@ import { BrandKitDisplay } from '@/components/brand-kit-display';
 import { generateBrandKitAction } from '@/app/actions';
 import { useToast } from '@/hooks/use-toast';
 import { AppLogo } from '@/components/app-logo';
-import { Sun } from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { useTheme } from 'next-themes';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [brandKit, setBrandKit] = useState<BrandKitOutput | null>(null);
   const { toast } = useToast();
+  const { setTheme } = useTheme();
 
   const handleFormSubmit = async (data: FormValues, file?: File) => {
     setIsLoading(true);
@@ -54,17 +62,33 @@ export default function Home() {
     <div className="min-h-screen w-full">
       <header className="flex items-center justify-between p-4 border-b border-border/40">
         <AppLogo />
-        <Button variant="ghost" size="icon">
-            <Sun className="h-5 w-5" />
-            <span className="sr-only">Toggle theme</span>
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </header>
       <main className="container mx-auto p-4 md:p-8 lg:p-12 xl:p-16">
         <div className="flex flex-col lg:flex-row lg:gap-12 xl:gap-24">
-          <div className="w-full lg:w-4/12 flex-shrink-0">
+          <div className="w-full flex-shrink-0 lg:w-[35%]">
             <BrandKitForm onSubmit={handleFormSubmit} isLoading={isLoading} />
           </div>
-          <div className="w-full mt-10 lg:mt-0 lg:w-8/12">
+          <div className="w-full mt-10 lg:mt-0">
             <BrandKitDisplay brandKit={brandKit} isLoading={isLoading} />
           </div>
         </div>
